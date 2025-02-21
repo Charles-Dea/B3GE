876ce492e264934626eb215e7e9185fa47328c72
Charles Dea <charliedea@proton.me>
2025-02-21 16:37:58 -0600

    fixed test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import engine
 engine.init(1024,'B3GE')
-engine.load_texture(20)
+engine.load_texture('test.png')
 sprites=[engine.Sprite(-.25,.25,0,1,1,1),engine.Sprite(.25,-.25,0,1,1,1)]
 for i in range(100):
     engine.draw(sprites)
