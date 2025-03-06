e9d537fb622d42058e0e5318ee0947e2f74a6568
Charles Dea <charliedea@proton.me>
2025-03-06 14:24:28 -0600

    ported to ubuntu 24

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python
+#!/bin/python3
 import engine
 engine.init(1024,'B3GE')
 engine.load_texture('test.png')
 sprites=[engine.Sprite(-.25,.25,0,1,1,1),engine.Sprite(.25,-.25,1,1,1,1)]
-for i in range(1000000000000):
+for i in range(100):
     engine.draw(sprites)
